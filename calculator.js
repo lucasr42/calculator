0503b555e706d41695de648e5ceb5e0152bdf451
Lucas Robinson <lucas.robinson42@hotmail.com>
2025-05-15 08:51:51 -0700

    Added the number and operator onclick handler that adds the choices to the screen

--- calculator.js
+++ calculator.js
@@ -24,28 +24,52 @@ const divide = (num1, num2) => {
 };
 
 /**
+ * Number and operator onClick handler
+ * Adds the number or operator clicked by the user to the screen
+ * @param {Obj} input Event object for onclick to access the number or operator
+ */
+const getUserInput = (input) => {
+    const inputDiv = document.querySelector("#userInput");
+    const inputVal = document.createElement("span");
+    inputVal.textContent = input.target.textContent;
+    inputDiv.appendChild(inputVal);
+}
+
+/**
  * Builds the number pad for users to click numbers
  * Need to add an eventListener and a function to take the numbers and do something with them
  */
 const buildNums = () => {
     let num = 1;
-    const operators = ["+", "-", "*", "/", "="];
+    const operators = ["+", "-", "*", "/"];
     const numberBoard = document.querySelector("#numberBoard");
+    
+    // Build the number board
     for (let i = 0; i < 3; i++) {
         for (let j = 1; j <= 3; j++) {
             const button = document.createElement("button");
-            button.id = "number";
+            buttonclassName = "number";
+            button.id = num;
             button.textContent = num;
+
+            // Add the click event listener
+            button.addEventListener("click", getUserInput);
+
             numberBoard.appendChild(button);
             num++;
         }
         const lineBreak = document.createElement("br");
         numberBoard.appendChild(lineBreak);
     }
+    
+    // Add the operators to the calculator board
     operators.forEach((operator) => {
         const operatorButton = document.createElement("button");
         operatorButton.id = operator;
         operatorButton.textContent = operator;
+
+        operatorButton.addEventListener("click", getUserInput);
+
         numberBoard.appendChild(operatorButton);
     })
 }
